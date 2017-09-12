d565910db5276bfa3b7e0fe9d134e3523f706edc
Kshitij Gupta <kshitijgm@gmail.com>
2017-09-12 13:26:25 +0000

    core: add/remove some packages
    Change-Id: Ia5033aa2d976d35619f9fc1c18415d5aa428c0c8
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
     BuiltInPrintService \
     Calendar \
     CalendarProvider \
@@ -42,7 +41,7 @@ PRODUCT_PACKAGES += \
     KeyChain \
     Keyguard \
     LatinIME \
-    Launcher2 \
+    Launcher3 \
     ManagedProvisioning \
     MtpDocumentsProvider \
     PicoTts \
@@ -51,14 +50,16 @@ PRODUCT_PACKAGES += \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
+    Stk \
     StorageManager \
     Telecom \
     TeleService \
+    Terminal \
     VpnDialogs \
     vr \
+    WallpaperPicker \
     MmsService
 
 # Themes
