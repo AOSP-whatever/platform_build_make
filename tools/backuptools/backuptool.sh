569c10a1a6f118d07643e48a9d5491f26e31981e
RahifM <faizel326@gmail.com>
2021-03-02 01:18:34 +0530

    TEMP
    Signed-off-by: RahifM <faizel326@gmail.com>

--- tools/backuptools/backuptool.sh
+++ tools/backuptools/backuptool.sh
@@ -6,7 +6,7 @@
 export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=18.1
+#export V=18.1
 
 export ADDOND_VERSION=3
 
@@ -47,10 +47,10 @@ if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
-  echo "Backup/restore is not possible. Incompatible ROM version: $V"
-  return 2
-fi
+#if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
+ # echo "Backup/restore is not possible. Incompatible ROM version: $V"
+  #return 2
+#fi
 return 0
 }
 
